9a7930f13fead9be918806fb83f29c9bc344b357
Siny <sinyozz.developer@gmail.com>
2024-05-09 12:44:48 -0700

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,14 @@
 /*global module:false*/
 module.exports = function(grunt) {
+    var sourceFiles = [
+        'js/game.js',
+        'js/entities/entities.js',
+        'js/entities/HUD.js',
+        'js/screens/title.js',
+        'js/screens/play.js',
+        'js/screens/gameover.js',
+    ];
+
     // Project configuration.
     grunt.initConfig({
         uglify: {
@@ -9,21 +18,56 @@ module.exports = function(grunt) {
             },
             dist: {
                 files: {
-                    'dist/clumsy-min.js': [
-                        'js/game.js',
-                        'js/entities/entities.js',
-                        'js/entities/HUD.js',
-                        'js/screens/title.js',
-                        'js/screens/play.js',
-                        'js/screens/gameover.js',
+                    'build/clumsy-min.js': [
+                        sourceFiles
                     ]
                 }
             }
-        }
+        },
+
+        jshint: {
+            options: {
+                jshintrc: ".jshintrc"
+            },
+
+            beforeConcat: {
+                files: {
+                    src: sourceFiles
+                }
+            },
+
+            afterConcat: {
+                files: {
+                    src: [ sourceFiles ]
+                }
+            }
+        },
+
+        connect : {
+            root : {
+                options : {
+                    port : process.env.PORT || 8001,
+                    keepalive : true,
+                    host: '*'
+                }
+            }
+        },
+
+        clean: {
+            dist: [
+                'build/clumsy-min.js'
+            ],
+        },
+
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks("grunt-contrib-connect");
+
 
     // Default task.
     grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('lint', ['jshint:beforeConcat','jshint:afterConcat']);
 };
